3875ed6fb4554576a5a4e6ea0da9f2ec6226c54a
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-01 08:26:24 +0700

    Add routes using controller functions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const game_controller = require("../controllers/gameController");
+const category_controller = require("../controllers/categoryController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", game_controller.index);
+
+/// GAME ROUTES ///
+
+router.get("/game/create", game_controller.game_create_get);
+
+router.post("/game/create", game_controller.game_create_post);
+
+router.get("/game/:id/delete", game_controller.game_delete_get);
+
+router.post("/game/:id/delete", game_controller.game_delete_post);
+
+router.get("/game/:id/update", game_controller.game_update_get);
+
+router.post("/game/:id/update", game_controller.game_update_post);
+
+router.get("/games/", game_controller.game_list);
+
+router.get("/game/:id", game_controller.game_detail);
+
+
+/// CATEGORY ROUTES ///
+
+router.get("/category/create", category_controller.category_create_get);
+
+router.post("/category/create", category_controller.category_create_post);
+
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+router.get("/category/:id/update", category_controller.category_update_get);
+
+router.post("/category/:id/update", category_controller.category_update_post);
+
+router.get("/categories/", category_controller.category_list);
+
+router.get("/category/:id", category_controller.category_detail);
 
 module.exports = router;
