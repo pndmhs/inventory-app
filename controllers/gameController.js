768d3fa7223b8c1b9d43d573beb62ce03544efe9
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-03 20:13:55 +0700

    Add get function to display form

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,18 +1,18 @@
 const Game = require("../models/game");
-const Category = require("../models/category")
+const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const [numGames, numCategories] = await Promise.all([
     Game.countDocuments({}).exec(),
-    Category.countDocuments({}).exec()
+    Category.countDocuments({}).exec(),
   ]);
 
   res.render("index", {
     title: "Game Inventory Home",
     game_count: numGames,
-    category_count: numCategories
-  })
+    category_count: numCategories,
+  });
 });
 
 exports.game_list = asyncHandler(async (req, res, next) => {
@@ -22,11 +22,12 @@ exports.game_list = asyncHandler(async (req, res, next) => {
 
 exports.game_detail = asyncHandler(async (req, res, next) => {
   const game = await Game.findById(req.params.id).populate("category").exec();
-  res.render("game_detail", { title: game.title, game: game })
+  res.render("game_detail", { title: game.title, game: game });
 });
 
 exports.game_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Game create GET");
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+  res.render("game_form", { title: "Add New Game", categories: allCategories });
 });
 
 exports.game_create_post = asyncHandler(async (req, res, next) => {
