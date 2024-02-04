a6f672889bf7487d53f65f8672a99bd8949bc850
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-04 14:44:21 +0700

    Add game delete get function

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -89,7 +89,16 @@ exports.game_create_post = [
 ];
 
 exports.game_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Game delete GET");
+  const game = await Game.findById(req.params.id).populate("category").exec();
+
+  if (game === null) {
+    res.redirect("/games");
+  }
+
+  res.render("game_delete", {
+    title: "Delete Game",
+    game: game,
+  });
 });
 
 exports.game_delete_post = asyncHandler(async (req, res, next) => {
